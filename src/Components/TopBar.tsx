d727bde79fcc8f582e7799a167841e3fae2b29d5
ilp0 <ilpo.loikkanen@gmail.com>
2022-11-27 19:49:45 +0200

    General style changes + add sublegends to keymap and fix keymap resize

--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import hidergoLogo from '../assets/hidergo_logo.png';
 import Device from '../Device';
 
-const TopBarContainer : React.CSSProperties = {
+const TopBarContainer: React.CSSProperties = {
     background: '#373737',
     padding: 10,
     margin: 0,
@@ -12,14 +12,16 @@ const TopBarContainer : React.CSSProperties = {
     maxHeight: 48,
     flexDirection: 'row',
     color: '#FFF',
-    
-    fontWeight: 800,
-    fontFamily: 'AlumniSansPinstripe',
+
+    fontWeight: 400,
+    fontFamily: 'Inter',
 };
 
-const topBarButton : React.CSSProperties = {
+const topBarButton: React.CSSProperties = {
+    paddingTop: "0.5em",
+    height: "100%",
     textAlign: 'center',
-    fontSize: '2em',
+    fontSize: '1.5em',
     flex: 1,
     cursor: 'pointer',
     borderRight: '1px solid #888',
@@ -29,7 +31,7 @@ type OnChangeViewCallback = (view: string) => any;
 type OnSelectDeviceCallback = (dev: Device | null) => any;
 
 
-export default function TopBar (props: {onChangeView: OnChangeViewCallback, onSelectDevice: OnSelectDeviceCallback}) {
+export default function TopBar(props: { onChangeView: OnChangeViewCallback, onSelectDevice: OnSelectDeviceCallback }) {
 
     const [devices, setDevices] = useState([...Device.devices]);
 
@@ -38,7 +40,7 @@ export default function TopBar (props: {onChangeView: OnChangeViewCallback, onSe
     useEffect(() => {
         setDevices([...Device.devices]);
         setSelectedDevice(Device.selectedDevice);
-        Device.addDeviceUpdateListener((d) => {
+        Device.addDeviceUpdateListener(() => {
             setDevices([...Device.devices]);
             setSelectedDevice(Device.selectedDevice);
         })
@@ -46,37 +48,33 @@ export default function TopBar (props: {onChangeView: OnChangeViewCallback, onSe
 
     return (
         <div style={TopBarContainer}>
-            <div style={{flex: 1, display: 'flex'}}>
-                <img src={hidergoLogo} style={{ flex: 1, maxWidth: 100}} />
+            <div style={{ flex: 1, display: 'flex', justifyContent: "space-around" }}>
+                <img src={hidergoLogo} style={{ flex: 1, maxWidth: 100 }} />
             </div>
-            <div style={{flex: 8, padding: 0, margin: 0}}>
-                <div style={{display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center', height: '100%'}}>
-                    <div style={topBarButton}>  
-                        <p style={{padding: 0, margin: 0}} onClick={() => {props.onChangeView("keymapeditor")}}>
-                            Keymap
-                        </p>
-                    </div>
-                    <div style={topBarButton} onClick={() => {props.onChangeView("display")}}>
-                        <p style={{padding: 0, margin: 0}}>
-                            Display
-                        </p>
-                    </div>
-                    <div style={{...topBarButton, ...{borderRight: 'none'}}} onClick={() => {props.onChangeView("trackpad")}}>
-                        <p style={{padding: 0, margin: 0}}>
-                            Trackpad
-                        </p>
-                    </div>
-                </div>
+            <div style={topBarButton}>
+                <p style={{ padding: 0, margin: 0 }} onClick={() => { props.onChangeView("keymapeditor") }}>
+                    Keymap
+                </p>
+            </div>
+            <div style={topBarButton} onClick={() => { props.onChangeView("display") }}>
+                <p style={{ padding: 0, margin: 0 }}>
+                    Display
+                </p>
+            </div>
+            <div style={{ ...topBarButton, ...{ borderRight: 'none' } }} onClick={() => { props.onChangeView("trackpad") }}>
+                <p style={{ padding: 0, margin: 0 }}>
+                    Trackpad
+                </p>
             </div>
             {
                 devices.length > 0 &&
-                <div style={{flex: 1}}>
-                    <InputLabel sx={{color: 'white'}} id="device-select-label">Device</InputLabel>
+                <div style={{ flex: 1, marginLeft: 10 }}>
+                    <InputLabel sx={{ color: 'white' }} id="device-select-label">Device</InputLabel>
                     <Select
                         label="Device"
                         labelId="device-select-label"
                         value={selectedDevice?.deviceInfo.device.serial}
-                        sx={{height: 30, color: 'white'}}
+                        sx={{ height: 30, color: 'white' }}
                         onChange={(e) => {
                             const _dev = devices.find(d => d.deviceInfo.device.serial === e.target.value) || null;
                             setSelectedDevice(_dev);
@@ -93,7 +91,7 @@ export default function TopBar (props: {onChangeView: OnChangeViewCallback, onSe
                     </Select>
                 </div>
             }
-            
+
         </div>
     )
 
