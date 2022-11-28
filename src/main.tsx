7c53ce79a473e2b7aa9ad6f33ef9ea7ece40c966
Oskari Seppa <oskari.seppa@haksi.fi>
2022-11-28 18:36:38 +0200

    Fixed useEffect running twice

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
